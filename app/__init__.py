8cf9abd95de8884ba0ff18763d2a233cca056537
ballicipluck <shyam4564@gmail.com>
2023-08-18 01:42:30 +0530

    app, waitress

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,16 @@
-from flask import Flask
+import os
+from pyrogram import Client
+from dotenv import load_dotenv
 
-web = Flask(__name__)
+load_dotenv()
+
+bot_token = os.environ.get('BOT_TOKEN', None)
+if bot_token is None:
+    raise Exception("BOT_TOKEN not found in environment variables")
+    exit(1)
+bot = Client(
+    "my_bot",
+    bot_token=bot_token,
+    api_id=os.environ.get('API_ID', None),
+    api_hash=os.environ.get('API_HASH', None)
+)
--- app/__main__.py
+++ app/__main__.py
@@ -1,15 +1,34 @@
-from . import web
+import threading
 import os
+
+from app import bot
+
+from flask import Flask
+from pyrogram import idle
+from waitress import serve
+
+web = Flask(__name__)
+
 @web.route('/', methods=['GET'])
 def index():
     return 'Hello World!'
 
 @web.route('/test')
 def test():
+    bot.send_message(chat_id='@hawardwolowitzs', text='Hey there!')
     return 'Test'
 
+# threading.Thread(target=web.run, daemon=True).start()
+# bot.start()
+# idle()
+
+def web_main():
+    serve(web, port=5000)
 
 if __name__ == '__main__':
+    print('Starting bot')
+    # start waitress in a thread and idle pyrogram in main thread
     port = int(os.environ.get('PORT', 5000))
-    print(f"--Running on port {port}--")
-    web.run(debug=True, port=port) # Dont use debug=True in production
+    threading.Thread(target=web_main, daemon=True).start()
+    bot.start()
+    idle()
