0056ac4e2bc7efc8a94c3bda00ab57f9e75cff96
ballicipluck <shyam4564@gmail.com>
2023-08-18 00:38:11 +0530

    .

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,5 @@
 from . import web
-
+import os
 @web.route('/', methods=['GET'])
 def index():
     return 'Hello World!'
@@ -10,4 +10,6 @@ def test():
 
 
 if __name__ == '__main__':
-    web.run(debug=True) # Dont use debug=True in production
+    port = int(os.environ.get('PORT', 5000))
+    print(f"--Running on port {port}--")
+    web.run(debug=True, port=port) # Dont use debug=True in production
